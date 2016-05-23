d71a2f10b52b3a63446ea15cc742209b474113de
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2016-05-23 22:20:56 +0000

    still working on MP, only missed percentage of votes when elected

--- index.js
+++ index.js
@@ -16,27 +16,28 @@ What they're doing - activity feed
 
 */
 
-var request = require('request');
+var request = require("request");
+var unaccented = require("./src/js/helper-function/unaccented.js");
 
 //The user input his postal code.  With this data, we request the name of his MP
 
 //We have to validate that the postal code is all upperCase, that it has only 6 characters without white space and that these characters follow LNL NLN
 //inside react, this coulb be an handleResult function 
 function handelResult (postalCode) {
-  postalCode.toUpperCase().replace(/\s+/g, '');
+  postalCode.toUpperCase().replace(/\s+/g, "");
     if(postalCode.length !== 6){
-      return 'invalid postal code';
+      return "invalid postal code";
     }
     else{
       var valid = /([ABCEGHJKLMNPRSTVXY]\d)([ABCEGHJKLMNPRSTVWXYZ]\d){2}/i;
       var ok = valid.test(postalCode);
       if(!ok){
-        return 'invalid postal code';
+        return "invalid postal code";
       }
       else{
         //At this point, we have a valid postal code, so we look for the name of the MP
-        console.log(postalCode);
         var findMPbyPC = `http://represent.opennorth.ca/postcodes/${postalCode}/?sets=federal-electoral-districts`;
+        console.log(findMPbyPC);
         request(findMPbyPC, function(err, result) {
           if(err){
             
@@ -44,7 +45,11 @@ function handelResult (postalCode) {
           else{
             var mpName = JSON.parse(result.body);
             var name = mpName.representatives_centroid[0].name;
-            var nameFormatted = name.toLowerCase().replace(/\s+/g, '-');
+            console.log(name, "name line 49");
+            //we have to formatted the name to be sure that is all lowercase, without accent and with a dash between firstname and lastname
+          var nameFormatted = unaccented.unaccented(name); 
+          console.log(nameFormatted, "nameFormatted line 52");
+          }
             //with the name of MP formatted, we fetch his data
             var findMP = `https://api.openparliament.ca/politicians/${nameFormatted}/?format=json`
             request(findMP, function(err, result) {
@@ -53,18 +58,20 @@ function handelResult (postalCode) {
               var party = mp.memberships[0].party.short_name.en;
               var image = mp.image; // https://api.openparliament.ca${image};
               var electedOn = mp.memberships[0].start_date;
-              var howManyVote = x;
+              var howManyVote = "x";
               var favoriteWord = mp.other_info.favourite_word[0];
               
               console.log(`https://api.openparliament.ca${image}`);
             
             });
         
-          }
-
         });
+
       }
-    }
+    }  
 }
 
 handelResult('H4R1E9');
+
+
+
