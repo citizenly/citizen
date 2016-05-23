68b686cde77cf3b049300ceb6f8edbd2aa9f7c86
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-05-23 17:19:43 +0000

    home and rep page freamwork with placeholder data

--- src/js/components/Rep.js
+++ src/js/components/Rep.js
@@ -3,7 +3,7 @@ var React = require('react');
 var Rep = React.createClass({
   getInitialState: function() {
     // get initial state with rep's info based on postcode
-    // retrieve from openparliament.ca API
+    // retrieve from openparliament.ca API - https://api.openparliament.ca/
     var rep = rep || {};
     return {
       rep: {
@@ -11,7 +11,7 @@ var Rep = React.createClass({
         constituency: rep.constituency || '**Papineau**',
         province: rep.province || '**QC**',
         party: rep.party || '**Liberal**',
-        img: rep.img || '**img.jpg**',
+        img: rep.img || '../images/567_1.jpg',
         electedYear: rep.electedYear || '**2015**',
         electedVotes: rep.electedVotes || '**52%**',
       },
@@ -20,6 +20,7 @@ var Rep = React.createClass({
   render: function() {
     return (
       <div className="main repInfoPage">
+        <img src={this.state.rep.img} />
         <h3>You are being represented by:</h3>
         <h1>{this.state.rep.name}</h1>
         <p>{this.state.rep.party} MP for {this.state.rep.constituency} ({this.state.rep.province})</p>
