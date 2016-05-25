062f717bac63e2f665d5278dc3e741994cdb0881
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-05-25 16:07:49 +0000

    fixed indents

--- src/js/components/Rep.js
+++ src/js/components/Rep.js
@@ -38,58 +38,55 @@ var Rep = React.createClass({
   },
   render: function() {
     return (
-
       <div className="rep-container">
-      
-          <div className="rep-info">
-                <div className="rep-pic">
-                    <img src={this.state.rep.img} />
-                </div>
-                    
-                <div className="rep-text">
-                    <h3>You are being represented by:</h3>
-                    <h1>{this.state.rep.name}</h1>
-                    <p>{this.state.rep.party} MP for {this.state.rep.constituency} ({this.state.rep.province})</p>
-                    <p>Won in {this.state.rep.electedYear} with {this.state.rep.electedVotes}% of the vote</p>
-                </div>
+        <div className="rep-info">
+          <div className="rep-pic">
+            <img src={this.state.rep.img} />
           </div>
       
-      <div className="rep-stats-container">
+          <div className="rep-text">
+            <h3>You are being represented by:</h3>
+            <h1>{this.state.rep.name}</h1>
+            <p>{this.state.rep.party} MP for {this.state.rep.constituency} ({this.state.rep.province})</p>
+            <p>Won in {this.state.rep.electedYear} with {this.state.rep.electedVote}% of the vote</p>
+          </div>
+        </div>
       
+        <div className="rep-stats-container">
           <div>
-                <p>In agreement with...</p>
+            <p>In agreement with...</p>
           </div>
           
           <div>
-                <p>you</p>
-                <p>**?**</p>
+            <p>you</p>
+            <p>**?**</p>
           </div>
           
           <div>
-                <p>your neighbours</p>
-                <p>**34%**</p>
+            <p>your neighbours</p>
+            <p>**34%**</p>
           </div>
           
           <div>
-                <p>... in her voting.</p>
+            <p>... in her voting.</p>
           </div>
-      </div>
+        </div>
       
-      <footer>
-        <div className="bottomMenu-item compare">
-          <Link to="/compare" activeClassName="active">COMPARE</Link>
-        </div>
-        <div className="bottomMenu-item petitions">
-          <Link to="/petitions" activeClassName="active">PETITIONS</Link>
-        </div>
-        <div className="bottomMenu-item feed">
-          <Link to="/feed" activeClassName="active">WHAT THEY'RE DOING</Link>
-        </div>
-        <div className="bottomMenu-item bills">
-          <Link to="/bills" activeClassName="active">WHAT WOULD YOU DO?</Link>
-        </div>
-     </footer>
-  </div>
+        <footer>
+          <div className="bottomMenu-item compare">
+            <Link to="/compare" activeClassName="active">COMPARE</Link>
+          </div>
+          <div className="bottomMenu-item petitions">
+            <Link to="/petitions" activeClassName="active">PETITIONS</Link>
+          </div>
+          <div className="bottomMenu-item feed">
+            <Link to="/feed" activeClassName="active">WHAT THEY'RE DOING</Link>
+          </div>
+          <div className="bottomMenu-item bills">
+            <Link to="/bills" activeClassName="active">WHAT WOULD YOU DO?</Link>
+          </div>
+       </footer>
+    </div>
 
     );
   }
