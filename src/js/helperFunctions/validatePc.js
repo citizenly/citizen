56bb5b8b0b861a9312e17551814442de13cda6ba
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2016-05-25 15:38:39 +0000

    changed the validation of postal code in RepAPI.js and added a redirect in server.js in case of invalid postal code

--- src/js/helperFunctions/validatePc.js
+++ src/js/helperFunctions/validatePc.js
@@ -1,6 +1,6 @@
 /*We have to validate that the postal code is all upperCase, 
 that it has only 6 characters without white space nor dash
-and that these characters follow LNL NLN
+and that these characters follow LNL NLN and have the valide letters
 */
 
 function validatePC(postcode) {
