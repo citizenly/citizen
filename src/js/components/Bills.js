64c440bdeb15215be7be7686e48fc865b98bfbef
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2016-05-26 00:50:30 +0000

    fetching infos about bills and setting the limit of results to the highest number of votes...still working on fetching the name of the bill

--- src/js/components/Bills.js
+++ src/js/components/Bills.js
@@ -2,79 +2,52 @@ var React = require('react');
 //var Link = require('react-router').Link;
 // required for ajax calls
 var axios = require('axios');
+import { withRouter } from 'react-router';
 
 
 var Bills = React.createClass({
   getInitialState: function() {
-    // set inital state as an empty array of objects, to be populated with info on each bill on componentDidMount
+    // set inital state to determine which list of bills is displayed - active by default
     return {
-      [{
-        id: "**C-14**",
-        title: "**An Act to amend the Criminal Code and to make related amendments to other Acts (medical assistance in dying)**",
-        status: "**Bill Not Active**",
-        lastVote: "**Passed**",
-        proposedBy: "**Jody Wilson-Raybould**",
-        repsVote: "**Didn't vote**",
-        date: "**2016-05-12**"
-      },
-      {
-        id: "**C-10**",
-        title: "**An Act to amend the Air Canada Public Participation Act and to provide for certain other measures**",
-        status: "**Active**",
-        lastVote: "**Failed*",
-        proposedBy: "**Marc Garneau **",
-        repsVote: "**Yes**",
-        date: "**2016-05-11**"
-      },
-      {
-        id: "**C-6**",
-        title: "**An Act to amend the Citizenship Act and to make consequential amendments to another Act**",
-        status: "**Active**",
-        lastVote: "**Passed**",
-        proposedBy: "**John McCallum**",
-        repsVote: "**No**",
-        date: "**2016-05-02**"
-      }]
+      filter: "active",
+      billList: []
     };
   },
   componentDidMount: function() {
     var that = this;
-    // get info for each bill filters by active
-    var id = this.props.params.id;
-    axios.post('/billsget', {
-      id: id
+    // set filter as url parameter
+    var filter = this.state.filter;
+    var path = '/bills/' + filter;
+    this.props.router.push(path);
+    
+    // post filter to server and this.setState({billList: response.data})
+    axios.post('/postfilter', {
+      filter: filter
     })
-    // update this.state with the bill object
     .then(function(response) {
-      var updateData = that.state.bill;
+      var updateData = that.state.billList;
       updateData = response.data;
-       that.setState({bill: updateData});
+      that.setState({billList: updateData});
+      console.log(that.state.billList, 'that.state.billList');
+      loading: false;
     })
     .catch(function(response) {
-      console.log(response);
     });
   },
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var that = this;
-    var pc = this.refs.postalcode.value;
-    var userPostalCode = formattedPc.validatePC(pc);
-    if(userPostalCode === "invalid") {
-        this.setState({invalidPostalCode: "alert"});
-        event.emit('show_message', {message:"Please, enter a valid postal code"});
-    }
-    else {
-      axios.post('/repnameget', {
-        postalcode: userPostalCode
-      }).then(function(response) {
-        var path = '/rep/' + response.data;
-        that.props.router.push(path);
-      })
-      .catch(function(response) {
-        console.log(response, 'response');
-      });
-    }
-  },
+  // handleOnClick: function(e) {
+  //   e.preventDefault();
+  //   var that = this;
+  //   var filter = "";
+  //   axios.post('/postfilter', {
+  //     filter: ""
+  //   }).then(function(response) {
+  //     var path = '/bills/' + response.data;
+  //     that.props.router.push(path);
+  //   })
+  //   .catch(function(response) {
+  //     console.log(response, 'response');
+  //   });
+  // },
   render: function() {
     return (
 
@@ -82,79 +55,58 @@ var Bills = React.createClass({
         <div className="billInfo">
           <h3>What would you do?</h3>
           <h1>Bills</h1>
+          
+          <div className="searchbox">
+          </div>
+          
           <div className="billTags">
             <ul>
-              <li>{this.state.bill.status}</li>
-              <li>{this.state.bill.lastVote}</li>
-              <li>{this.state.bill.proposedBy}</li>
+              <li><button onClick={this.handleOnClick}></button>active</li>
+              <li>passed</li>
+              <li>failed</li>
+              <li>rep proposed</li>
+              <li>rep voted</li>
+              <li>all</li>
             </ul>
           </div>
-          <h2>{this.state.bill.id}</h2>
-          <h2>My representative voted: {this.state.bill.repsVote}</h2>
-        </div>
-
-        <div className="billTabs">
-          <ul className="tabs group">
-            <li><a href="#box-one">TITLE</a></li>
-            <li><a href="#box-two">SUMMARY</a></li>
-            <li><a href="#box-three">FULL TEXT</a></li>
-            <li><a href="#box-four">HOUSE DEBATE</a></li>
-          </ul>
-
-      		<div className="box-wrap">
-        		<div id="box-one">
-        		  <h3>{this.state.bill.title}</h3>
-        		</div>
-        		
-        		<div id="box-two">
-        		  <h3>{this.state.bill.summary}</h3>
-        		</div>
-        		
-        		<div id="box-three">
-        		  <h3>{this.state.bill.text}</h3>
-        		</div>
-        
-            <div id="box-four">
-        		  <h3>{this.state.bill.houseDebate}</h3>
-        		</div>
-        	</div>
-        	
+          <h2>bill.id</h2>
+          <h2>My representative voted: repsVote</h2>
         </div>
         
-        <div className="voteCompare">
-          <div><p>**Whole Country**</p></div>
-          <div><p>**Your Neighbours**</p></div>
-        </div>
+        {this.state.billList.map(function(pizza){
+                  return (
+          <h1>bill list: {this.state.billList}</h1>
 
-        <div className="vottingButtons">
-          <div>
-            <div className="arrow">
-              <p>arrow</p>
-            </div>
-          </div>
-          
-          <div>
-            <div>
-        	  	<img className= "responsive-img" src="images/red button.png"></img>
-            </div>
-          </div>
-          
-          <div>
-            <div>
-          	 	<img className= "responsive-img" src="images/green button.png"></img>
-            </div>
-          </div>
-          
-          <div>
-            <div className="arrow">
-        			<p>arrow</p>
-            </div>
-          </div>
-          
-        </div>
       </div>
     );
   }
 });
 
-module.exports = Bills;
+module.exports = withRouter(Bills);
+
+
+      //{
+      //   id: "**C-14**",
+      //   title: "**An Act to amend the Criminal Code and to make related amendments to other Acts (medical assistance in dying)**",
+      //   status: "**Bill Not Active**",
+      //   lastVote: "**Passed**",
+      //   proposedBy: "**Jody Wilson-Raybould**",
+      //   repsVote: "**Didn't vote**",
+      //   date: "**2016-05-12**"
+      // }, {
+      //   id: "**C-10**",
+      //   title: "**An Act to amend the Air Canada Public Participation Act and to provide for certain other measures**",
+      //   status: "**Active**",
+      //   lastVote: "**Failed*",
+      //   proposedBy: "**Marc Garneau **",
+      //   repsVote: "**Yes**",
+      //   date: "**2016-05-11**"
+      // }, {
+      //   id: "**C-6**",
+      //   title: "**An Act to amend the Citizenship Act and to make consequential amendments to another Act**",
+      //   status: "**Active**",
+      //   lastVote: "**Passed**",
+      //   proposedBy: "**John McCallum**",
+      //   repsVote: "**No**",
+      //   date: "**2016-05-02**"
+      // }
---
+++ src/js/helperFunctions/billsAPI.js
@@ -0,0 +1,116 @@
+/*
+ID / official name
+Title
+Summary
+Full Text
+House Debate
+Status tag - active/inactive
+Proposed by
+*/
+
+
+var request = require("request");
+var findBillId = require("./findBillId.js")
+
+/*At this url there are all the votes of the current session of the current parlement
+'http://api.openparliament.ca/votes/?date=&session=42-1&format=json';
+By default, the limit of results by page is setting at 20 and the order of the result is starting by the highest.  
+As every vote in a session has a sequential number, we set the limit to 
+to the highest vote's number
+*/
+
+function fixLimitByPage(callback) {
+  var add = 'http://api.openparliament.ca/votes/?session=42-1&limit=1&format=json';
+  request(add, function(err, result){
+    if (err) {
+      callback(err);
+    }
+    else {
+      var lastVote = JSON.parse(result.body);
+      var numberLastVote = lastVote.objects[0].number;
+      callback(numberLastVote);
+    }
+  });
+}
+ 
+
+//Calling this function with a callback, we recive an arrry of votes
+function getAllVotes(limit, callback) {
+  var allVotes = `http://api.openparliament.ca/votes/?date=&session=42-1&limit=${limit}&format=json`;
+  request(allVotes, function(err, result) {
+    if (err) {
+      callback(err);
+    }
+    else {
+      var votes = JSON.parse(result.body);
+      var arrOfVotes = votes.objects;
+      callback(arrOfVotes);
+    }
+  });
+}
+
+function getListofBillsFromVotes(arrOfVotes, callback) {
+  var bills = [];
+  arrOfVotes.forEach(function(vote) {
+    var result = vote.result;
+    var date = vote.date;
+    var number = vote.number;
+    var billId = findBillId.findBillId(vote.bill_url);
+
+    var bill = {
+      result: result,
+      date: date,
+      number: number,
+      billId: billId
+    };
+
+    //Not all the votes is about a bill, so we want to keep only the bills
+    if (bill.billId.length > 0) {
+      bills.push(bill);
+    }
+
+  });
+  callback(bills);
+}
+
+function getTitleOfBill(bills, callback) {
+  bills.forEach(function(bill){
+    var 
+  })
+}
+
+function getUniqueBillsByDate(bills, callback) {
+
+  var bin = {};
+  var allBills = [];
+  bills.filter(function(obj) {
+    bin[obj.billId] = bin[obj.billId] || [];
+    bin[obj.billId].push(obj);
+  })
+  var filterdBills = [];
+  for (var bill in bin) {
+    var latestBill = bin[bill].reduce(function(prev, next) {
+      var x = new Date(prev.date);
+      var y = new Date(next.date);
+      if (x < y) {
+        return next;
+      }
+      else {
+        return prev;
+      }
+
+    })
+    allBills.push(latestBill);
+  }
+  callback(allBills);
+
+}
+
+
+module.exports = {
+  getAllVotes: getAllVotes,
+  getListofBillsFromVotes: getListofBillsFromVotes,
+  getUniqueBillsByDate: getUniqueBillsByDate,
+  fixLimitByPage: fixLimitByPage
+};
+
