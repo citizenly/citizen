15e217e0e257d6598693e9761a10a30a966075ea
Osa Isacson <asaisacson@gmail.com>
2018-02-08 18:32:44 +0000

    empty states for compare and style update to loader for bills

--- src/js/components/Compare.js
+++ src/js/components/Compare.js
@@ -19,13 +19,14 @@ var Bill = React.createClass({
     return (
       <div className="listedItem">
         <Link to={"/singlebill/" + this.props.billId}>
+          <div className="sub-h2">{this.props.billId}</div>
           <div className="one-line-spread">
-            <div className="sub-h2">{this.props.billId}</div>
-            { ( this.props.params.filter === "votedonbymyrep" &&  this.props.ballot) ? <h4 className={resultClass}>{result}</h4> : "" }
+            <h4>Latest status in parliament: </h4>
+            { ( this.props.params.filter === "votedonbymyrep" &&  this.props.ballot) ? <h4 className={resultClass}>{result}</h4> : <h4 class="dynamicDi">No action yet</h4> }
           </div>
           <div className="one-line-spread">
-          <h4>My representative's action - </h4>
-          { (this.props.params.filter === "votedonbymyrep" &&  this.props.ballot) ?  <h4 className={ballotClass}>{ballot}</h4> : "" } 
+          <h4>My representative voted: </h4>
+          { (this.props.params.filter === "votedonbymyrep" &&  this.props.ballot) ?  <h4 className={ballotClass}>{ballot}</h4> : <h4 class="dynamicDi">No action yet</h4> } 
           </div>
           <h5>{this.props.billTitle}</h5>
         </Link>
@@ -88,10 +89,9 @@ var Compare = React.createClass({
       <div className="comparePage">
             <div className="centered-container">
               <div className="top-h2">all bills</div>
-            </div>
-            
-          {this.props.params.filter === "votedonbymyrep"  ? <p>The below shows the bills {this.state.repFullName} has voted on. <span className="dynamicYe">PASSED</span>/<span className="dynamicNo">FAILED</span>/<span className="dynamicDi">TIE</span> indicates the bill's latest status in parliament</p>  : ""} 
-          {this.props.params.filter === "proposedbymyrep"  ? <p>The below shows bills your representative {this.state.repFullName} has proposed.</p>  : ""} 
+              {this.props.params.filter === "votedonbymyrep"  ? <p>The below shows the bills {this.state.repFullName} has voted on.</p>  : ""} 
+              {this.props.params.filter === "proposedbymyrep"  ? <p>The below shows bills your representative {this.state.repFullName} has proposed.</p>  : ""} 
+          </div>
         <div className="filters">
           <div><Link activeClassName="active" to="/compare/votedonbymyrep">New votes</Link></div>
           <div><Link activeClassName="active" to="/compare/proposedbymyrep">Proposed</Link></div>
